574786eaf3d2d0bc21c779c841375fc594e5ec14
Yididiya Deneke <yididyadene@gmail .com>
2023-03-19 13:27:34 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ * Return: Always 1(success)
+ */
+int main(void)
+{
+	printf(""and that piece of art is useful" - Dora Korpar, 2015-10-19\n")
+return (1);
+}
