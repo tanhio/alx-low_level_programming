3e81b9d2c0f6387bbf5e4465de29459d46bede30
Yididiya Deneke <yididyadene@gmail .com>
2023-03-18 20:43:57 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -A program which prints a senetence 
+ *
+ * Return: The main function returns 0 if it works
+ * /
+ int main(void)
+ {
+	 puts("Programming is like building  multilingual puzzle");
+
+	 return (0);
+ }
