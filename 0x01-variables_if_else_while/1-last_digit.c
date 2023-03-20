3988d3676389857927fcb2ddda139170b5bc5da2
Yididiya Deneke <yididyadene@gmail .com>
2023-03-20 12:56:46 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and and is less than 6 and not 0\n"n, n % 10);
 	}
-	else
+	else if ((n % 10) == 0)
 	{
 		printf("Last digit of %d is 0\n"n, n % 10);
 	}
