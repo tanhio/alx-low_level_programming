c721a79f2e8d05daa06b45fdb4afaf7581834910
Yididiya Deneke <yididyadene@gmail .com>
2023-03-20 16:25:33 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Print the alphabets in lowercase
+ *Return: Always 0(success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i <= 'a'; i++)
+	putchar(i);
+	putchar('\n');
+return (0);
+}
