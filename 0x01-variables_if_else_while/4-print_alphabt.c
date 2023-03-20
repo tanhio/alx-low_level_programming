c8a0a101b3f175a9fde57d78e107ec1e47cac4ee
Yididiya Deneke <yididyadene@gmail .com>
2023-03-20 15:38:57 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Print all the letters except q and e except with lowercase
+ *Return: always 0(success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+		putchar(i);
+	}
+	putchar('/n');
+return (0);
+}
