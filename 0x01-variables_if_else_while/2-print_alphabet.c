a33b0de0c03b4e7591e4f079febef609fa0482fc
Yididiya Deneke <yididyadene@gmail .com>
2023-03-20 13:49:29 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Print the alphabets in lowercase
+ *Return: Always 0(success)
+ */
+int main(void)
+{
+	char c;
+
+	for (i = 'a'; i <= 'z'; i++)
+	putchar(i);
+	putchar('\n');
+return (0);
+}
