b29be41c9078e093ebc75eba36a1a051cef2f082
Yididiya Deneke <yididyadene@gmail .com>
2023-03-20 15:51:48 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Print all the letters except q and e except with lowercase
+ *Return: always 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	putchar(i);
+	putchar("/n");
+return (0);
+}
