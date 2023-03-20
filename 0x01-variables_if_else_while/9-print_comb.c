f888e9033e197bd1d969a204d37e754a18ee1cba
Yididiya Deneke <yididyadene@gmail .com>
2023-03-20 16:58:28 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Print the alphabets in lowercase
+ *Return: Always 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	putchar(i);
+		if (i != '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+return (0);
+}
