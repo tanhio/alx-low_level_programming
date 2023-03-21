a211d295ac3682c4d9af7e687028b872da311350
Yididiya Deneke <yididyadene@gmail .com>
2023-03-20 18:42:31 -0700

    9-print_comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,13 @@ int main(void)
 
 	for (i = '0'; i < '9'; i++)
 	{
+
 	for (j = i + 1; j <= '9'; j++)
 	{
 		if (i != j)
 		{
-	putchar(i);
-	putchar(j);
+		putchar(i);
+		putchar(j);
 		if (i == '8' && j == '9')
 		continue;
 		putchar(',');
