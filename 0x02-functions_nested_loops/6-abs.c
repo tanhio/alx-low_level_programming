219a691021d5b12ed92d1deb9b0a5158fe0f79b7
Yididiya Deneke <yididyadene@gmail .com>
2023-03-21 19:07:16 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer
+ * @i: The integer
+ * Return: Always i(success)
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+	i = i * -1;
+	}
+return (i);
+}
