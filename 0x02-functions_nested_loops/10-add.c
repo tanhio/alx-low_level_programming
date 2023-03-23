3e5502e4c880102b5b5c8f88e55b6480f03bd5f4
Yididiya Deneke <yididyadene@gmail .com>
2023-03-22 17:45:40 -0700

    ./10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *add - Adds two integers
+ *@i: The first added intrger
+ *@j: The second added intrger
+ *Returns: The sum
+ */
+int add(int i, int j)
+{
+	int k = i + j;
+return (k);
+}
