67067879b77e5c4f5928bf0adf097aaf6cd26c6f
Yididiya Deneke <yididyadene@gmail .com>
2023-03-23 16:07:31 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Start the program
  *
@@ -8,21 +6,11 @@
  */
 int main(void)
 {
-        int n;
+	int i;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        if (n > 0)
-        {
-                printf("%d is positive\n", n);
-        }
-        else if (n == 0)
-        {
-                printf("%d is zero\n", n);
-        }
-        else
-        {
-                printf("%d is negative\n", n);
-        }
+	i = 0;
+
+	positive_or_negative(i);
+
 return (0);
 }
