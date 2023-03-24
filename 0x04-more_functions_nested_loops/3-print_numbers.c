a209c66fd4467723774919fe536a05ce603d4608
Yididiya Deneke <yididyadene@gmail .com>
2023-03-23 20:03:33 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_numbers - Print the numbers from 0 upto 9
+ *Return: The numbers from 0 upto 9
+ */
+void  print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	_putchar(x + '0'(
+	}
+	_putchar('\n');
+}
