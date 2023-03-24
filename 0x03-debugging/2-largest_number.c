6f311c941a1c8b05d38b4f907476682241ed5e68
Yididiya Deneke <yididyadene@gmail .com>
2023-03-23 17:33:16 -0700

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,22 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && a >= c)
 	{
 	largest = a;
 	}
-	else if (b < a && a < c)
+	else if (b >= a && b >= c)
+	{
+	largest = b;
+	}
+	else if (c >= a && c >= b)
 	{
 	largest = c;
 	}
-	else if (b > c && c >)
-	{
-	 largest = b;
-	}
-	 else if (b < c && c > a)
-	{
-	largest = c;
-	}
-
-    return (largest);
+return (largest);
 }
