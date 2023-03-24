1910012bbbd47342510f1571b1d2b845ca22c6c1
Yididiya Deneke <yididyadene@gmail .com>
2023-03-24 11:29:50 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,9 +6,13 @@ void print_most_numbers(void)
 {
 	for (int i = 0; i < 10; i++)
 	{
-		if (i != 2 || i != 10)
+		if (i != 2 || i != 4)
 		{
 			_putchar(i + '0');
+		}
+		else
+		{
+			continue;
 		}
 			_putchar('\n');
 	}
