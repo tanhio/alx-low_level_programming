fd6ed556075a574029f8e94e364979c8685a5a6f
Yididiya Deneke <yididyadene@gmail .com>
2023-03-24 12:18:27 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+
+#include "main.h"
+/**
+ *more_numbers - Print 10 times the numbers, from 0 to 14
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 15; i++)
+	{
+		int j = 0;
+
+		while (j < 10)
+		{
+			if (i == 2 || i == 4)
+			{
+				continue;
+			}
+				else
+			{
+				_putchar(i + '0');
+			}
+			putchar('\n');
+			j++;
+		}
+	}
+}
