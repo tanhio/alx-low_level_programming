53ae98f3cad01269fa319ee1893b2e392e0b67c4
Yididiya Deneke <yididyadene@gmail .com>
2023-03-24 18:40:52 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *main - Print numbers with words
+ *Return: 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+		printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+		printf("buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+		printf("Fizz ");
+		}
+		else
+		{
+		printf("%d ", i);
+		}
+	}
+	printf("\n");
+return (0);
+}
