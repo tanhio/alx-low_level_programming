90867033c54b2fe89509f4a5e6d46a41f48e55a3
Yididiya Deneke <yididyadene@gmail .com>
2023-03-30 19:56:09 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @src: string
+ * Return: value of src
+ */
+
+char *leet(char *src)
+{
+	int i, j;
+	char s1[] = 'aAeEoOtTlL';
+	char s2[] = '4433007711';
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (src[i] == s1[j])
+			{
+				src[i] = s2[j];
+			}
+		}
+	}
+return (src);
+}
