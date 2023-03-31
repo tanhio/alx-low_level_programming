61eaf6c818d8613fb01686d18102582302abe882
Yididiya Deneke <yididyadene@gmail .com>
2023-03-30 19:59:39 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,8 +9,8 @@
 char *leet(char *src)
 {
 	int i, j;
-	char s1[] = 'aAeEoOtTlL';
-	char s2[] = '4433007711';
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
