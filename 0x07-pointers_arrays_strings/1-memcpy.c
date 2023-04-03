cb21d8b34b38c9d71486703cf1621d3fd26479f6
Yididiya Deneke <yididyadene@gmail .com>
2023-04-03 16:57:55 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: memory area
+ * @src: bytes from memory area
+ * @n:  function copies 
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+return (dest);
+}
