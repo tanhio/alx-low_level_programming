0abc19062709e714da6ab00a1a4556f0dfc1f2b6
Yididiya Deneke <yididyadene@gmail .com>
2023-04-10 18:39:14 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,32 +7,32 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, j = 0, k = 0, l = 0, m = 0, n = 0;
+	int i = 0, d = 0, n = 0, len = 0, f = 0, digit = 0;
 
-	while (s[l] != '\0')
-		l++;
+	while (s[len] != '\0')
+		len++;
 
-	while (i < l && m = 0)
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-			++j;
+			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			n = s[i] - '0';
-			if (j % 2)
-				n = -n;
-			k = k * 10 + n;
-			m = 1;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			m = 0;
+			f = 0;
 		}
 		i++;
 	}
-	if (m == 0)
+	if (f == 0)
 		return (0);
-return (k);
+return (n);
 }
 /**
  * main - multiplies two numbers
