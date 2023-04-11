311f31fc414685d2c9bb02720ab26b4f72ea272b
Yididiya Deneke <yididyadene@gmail .com>
2023-04-10 18:18:54 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * _atoi - converts a string to an integer
+ * @s: the string
+ * Return: the converted int
+ */
+int _atoi(char *s)
+{
+	int i = 0, j = 0, k = 0, l = 0, m = 0, n = 0;
+
+	while (s[l] != '\0')
+		l++;
+
+	while (i < l && m = 0)
+	{
+		if (s[i] == '-')
+			++j;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			n = s[i] - '0';
+			if (j % 2)
+				n = -n;
+			k = k * 10 + n;
+			m = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			m = 0;
+		}
+		i++;
+	}
+	if (m == 0)
+		return (0);
+return (k);
+}
+/**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
