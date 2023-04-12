49ddad0499854d0b2224ff3bb14cb90ec181999b
Yididiya Deneke <yididyadene@gmail .com>
2023-04-12 12:41:43 -0700

    1-strdup.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_array - creates an array of chars, and initializes it with a specific char
+ * create_array - creates an array of chars
  * @size: the size of array
  * @c: the char to assign
  * Return: a pointer to the array, or NULL if it fails
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - copy to new memory location
+ * @str: the character
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *cpStr;
+	int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	cpStr = malloc(sizeof(char) * (i + 1);
+
+			if (cpStr == NULL)
+				return (NULL);
+
+			for (j = 0; str[j]; j++)
+				cpStr[j] = str[j];
+
+return (cpStr);
+}
