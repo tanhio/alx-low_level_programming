42eac8ea762634edbcd2475b6dbc78acc2a95fbd
Yididiya Deneke <yididyadene@gmail .com>
2023-04-14 14:58:06 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr = malloc(nmemb * size);
 
 
-	if (j == 0 || ptr == NULL)
+	if (nmemb == 0 || size == 0 || ptr == NULL)
 	{
 		return (NULL);
 	}
