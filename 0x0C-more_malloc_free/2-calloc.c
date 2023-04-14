18f473d540ae270c6128481446f326b42bd27bca
Yididiya Deneke <yididyadene@gmail .com>
2023-04-14 16:10:55 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * _save - save a character in a memory
+ * @dest: the memory
+ * @c: the character
+ * @n: number characters to be copied
+ * Return: pointer to the memory
+ */
+char *_save(char *dest, char c, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = c;
+	}
+return (dest);
+}
+/**
  * _calloc -  allocates memory for an array, using malloc
  * @nmemb: an array
  * @size: the number of bytes to free in the allocared memory
@@ -8,25 +25,16 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int i, j = nmemb * size;
 	char *ptr;
 
-	ptr = malloc(j);
+	ptr = malloc(nmemb * size);
 
-	if (ptr == NULL)
+	if (ptr == NULL || nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 
-	if (nmemb == 0 || size == 0)
-	{
-		return (NULL);
-	}
-
-	for (i = 0; i < j; i++)
-	{
-		ptr[i] = 0;
-	}
+	_save(ptr, 0, (size * nmemb));
 return (ptr);
 }
 
