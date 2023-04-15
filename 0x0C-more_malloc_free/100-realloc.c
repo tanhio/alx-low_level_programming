ec36983c4a921e1b7d46e3e4bd23e3461309b1a0
Yididiya Deneke <yididyadene@gmail .com>
2023-04-14 18:44:36 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,8 +29,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	else
 	{
 		if (new_size == 0)
+		{
 			free(ptr);
 			return (NULL);
+		}
 	}
 	newptr = malloc(new_size);
 
