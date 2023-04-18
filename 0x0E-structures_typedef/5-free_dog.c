9a6165c2e75b4c10eb929421ce869b3869286a36
Yididiya Deneke <yididyadene@gmail .com>
2023-04-18 16:54:12 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dogs - frees dogs
+ * @d: a dog struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+		free((*d).name);
+		 free((*d).owner);
+		 free(d);
