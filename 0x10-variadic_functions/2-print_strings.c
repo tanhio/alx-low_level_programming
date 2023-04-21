aa2c8523f809709553b643c4ac2fd8fdb8a91aef
Yididiya Deneke <yididyadene@gmail .com>
2023-04-20 18:47:23 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -6,7 +6,7 @@
  * @separator: the string to be printed between numbers
  * @n: the number of integers passed to the function
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list fp;
 	unsigned int i;
