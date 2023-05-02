88bb61bfe979b02066e02dc3c4aec3a35b980b59
tanhio <yididyadene@gmail.com>
2023-05-02 12:55:31 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new_h;
 
 	new_h = malloc(sizeof(listint_t));
-	
+
 	if (!new_h)
 	{
 		return (NULL);
--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,8 +12,8 @@ int pop_listint(listint_t **head)
 	if (!*head || !head)
 		return (0);
 
-	i = (**head).n;
-	new = (**head).next;
+	i = (*head).n;
+	new = (*head).next;
 	free(*head);
 	*head = new;
 
