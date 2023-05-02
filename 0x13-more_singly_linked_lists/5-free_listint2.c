a511d587de9b5e971128fe066a9b08d6a77512de
tanhio <yididyadene@gmail.com>
2023-05-02 00:01:28 -0700

    6-get_nodeint.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,14 @@
 #include "lists.h"
 /**
  * free_listint2 - frees a listint_t list
- * @head: a pointer to the list
+ * @head: a pointer to the pointer the list
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *new;
+
+	if (head == NULL)
+		return;
 
 	while (*head)
 	{
---
+++ 0x13-more_singly_linked_lists/6-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes the head node of a listint_t linked list
+ * @head - a pointer to the pointer to the list
+ * Return: the deleted data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *new;
+	int i;
+
+	if (!*head || !head)
+		return (0);
+
+	i = (**head).n;
+	new = (**head).next;
+	free(*head);
+	free(*head);
+	*head = new;
+
+return (i);
+}
