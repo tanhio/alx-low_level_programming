38eea10d41aa50388677006da5afd153af4c96dd
tanhio <yididyadene@gmail.com>
2023-05-03 19:06:38 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -35,4 +35,6 @@ size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 size_t free_listint_safe(listint_t **h);
 size_t print_listint_safe(const listint_t *head);
+listint_t *find_listint_loop(listint_t *head);
+
 #endif
