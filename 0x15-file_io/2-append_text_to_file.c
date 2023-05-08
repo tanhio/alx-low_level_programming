fa6a54158b739bf608a348efc5eee7ae02272564
tanhio <yididyadene@gmail.com>
2023-05-08 16:03:26 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,9 +19,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	file = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	w = write(file, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (file == -1 || w == -1)
 		return (-1);
 
 	close(file);
